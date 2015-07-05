804f64f4a10e552e22344d08be64c526939ee86d
iok <iok@iok-PC>
2015-07-05 09:14:12 +0800

    add4

--- Profitmarket/gen/com/example/profitmarket/R.java
+++ Profitmarket/gen/com/example/profitmarket/R.java
@@ -23,24 +23,56 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int s_buycoupon=0x7f020001;
+        public static final int s_clearing=0x7f020002;
+        public static final int s_coupon=0x7f020003;
+        public static final int s_records=0x7f020004;
+        public static final int s_start_customer=0x7f020005;
+        public static final int s_start_stores=0x7f020006;
+        public static final int s_storephoto=0x7f020007;
+        public static final int s_stores=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080012;
+        public static final int button1=0x7f080010;
+        public static final int button2=0x7f080011;
+        public static final int editText1=0x7f08000d;
+        public static final int editText2=0x7f08000e;
+        public static final int editText3=0x7f08000f;
+        public static final int imageButton1=0x7f080000;
+        public static final int imageButton2=0x7f080001;
+        public static final int imageButton3=0x7f080007;
+        public static final int imageButton4=0x7f080006;
+        public static final int imageButton5=0x7f08000a;
+        public static final int imageView1=0x7f08000c;
+        public static final int linearLayout1=0x7f080002;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080009;
+        public static final int textView5=0x7f080008;
+        public static final int textView6=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int s_mainmenu=0x7f030001;
+        public static final int s_storescenter=0x7f030002;
+        public static final int s_storescenteramend=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
         public static final int s__mainmenu=0x7f070001;
+        public static final int s_storescenter=0x7f070002;
+        public static final int s_storescenteramend=0x7f070003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_s__mainmenu=0x7f050003;
+        public static final int companyname=0x7f050003;
+        public static final int hello_world=0x7f050005;
+        public static final int title_activity_s__mainmenu=0x7f050002;
+        public static final int title_activity_s_storescenter=0x7f050004;
+        public static final int title_activity_s_storescenteramend=0x7f050006;
     }
     public static final class style {
         /** 
