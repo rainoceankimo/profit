ee5db4b6be67540f50fe437f09cd773b5083bdcd
iok <iok@iok-PC>
2015-07-14 15:51:09 +0800

    iok714351

---
+++ Profitmarket/src/com/example/profitmarket/S_Records_InquirereTransactionre.java
@@ -0,0 +1,34 @@
+package com.example.profitmarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class S_Records_InquirereTransactionre extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.s_records_inquireretransaction);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.s__inquirere__transactionre_records, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
