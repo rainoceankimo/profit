ccad6175a901a62d6f0821c9d688dfebffa90a80
iok <iok@iok-PC>
2015-07-17 17:55:46 +0800

    iok717555

--- Profitmarket/src/com/example/profitmarket/S_Records_InquireTransactionre.java
+++ Profitmarket/src/com/example/profitmarket/S_Records_InquireTransactionre.java
@@ -1,7 +1,9 @@
 package com.example.profitmarket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -31,4 +33,21 @@ public class S_Records_InquireTransactionre extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            // Show home screen when pressing "back" button,
+            //  so that this app won't be closed accidentally
+        	Intent intent = new Intent();  
+    	    intent.setClass(S_Records_InquireTransactionre.this,S_Records.class);
+    	   startActivity(intent);    //Ä²µo´«­¶
+    	   finish();   //µ²§ô¥»­¶
+            
+            return true;
+        }
+        
+        return super.onKeyDown(keyCode, event);
+    }
 }
