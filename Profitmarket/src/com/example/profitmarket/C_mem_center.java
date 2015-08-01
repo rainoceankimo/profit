010697e5f15c77a045e68b61b2d4fbca974c83aa
iok <iok@iok-PC>
2015-08-01 23:02:12 +0800

    iok8.1

--- Profitmarket/src/com/example/profitmarket/C_mem_center.java
+++ Profitmarket/src/com/example/profitmarket/C_mem_center.java
@@ -1,19 +1,164 @@
 package com.example.profitmarket;
 
+import helper.SQLiteHandler;
+import helper.SessionManager;
+ 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class C_mem_center extends Activity {
-
+public class C_mem_center extends Activity implements SurfaceHolder.Callback
+{
+	
+	private EditText textName;
+    private EditText textEmail;
+    private Button QRcode;
+    
+    private SQLiteHandler db;
+    private SessionManager session;
+    private String TAG = "HIPPO";
+    private SurfaceView mSurfaceView01;
+    private SurfaceHolder mSurfaceHolder01;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_c_mem_center);
+		
+		textName = (EditText) findViewById(R.id.textname);
+        textEmail = (EditText) findViewById(R.id.textemail);
+        QRcode = (Button) findViewById(R.id.button2);
+        
+        /*眔秆猂钩*/
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        
+        mSurfaceView01 = (SurfaceView) findViewById(R.id.surfaceView1);
+        mSurfaceHolder01 = mSurfaceView01.getHolder();
+        mSurfaceHolder01.addCallback(C_mem_center.this);
+        
+        
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+ 
+        // session manager
+        session = new SessionManager(getApplicationContext());
+		
+        HashMap<String, String> user = db.getUserDetails();
+        
+        String name = user.get("name");
+        String email = user.get("email");
+ 
+        // Displaying the user details on the screen
+        textName.setText(name);
+        textEmail.setText(email);
+        
+        
+        //玻ネQR Code
+        QRcode = (Button)findViewById(R.id.button2);
+        QRcode.setOnClickListener(new Button.OnClickListener()
+        {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				if(QRcode.getText().toString()!="")
+		        {
+		          /* 传入setQrcodeVersion为4，仅能接受62个字符 */
+		          AndroidQREncode(textName.getText().toString(), 4);
+		        }
+				
+			}
+        });
+        
+        
+		
+	}
+	
+	public void AndroidQREncode(String strEncoding, int qrcodeVersion)
+	{
+	    try
+	    {
+	      /* 建构QRCode编码对象 */
+	      com.swetake.util.Qrcode testQrcode = new com.swetake.util.Qrcode();
+	      /* L','M','Q','H' */
+	      testQrcode.setQrcodeErrorCorrect('M');
+	      /* "N","A" or other */
+	      testQrcode.setQrcodeEncodeMode('B');
+	      /* 0-20 */
+	      testQrcode.setQrcodeVersion(qrcodeVersion);
+	      
+	      // getBytes
+	      byte[] bytesEncoding = strEncoding.getBytes("utf-8");
+	      
+	      if (bytesEncoding.length>0 && bytesEncoding.length <256)
+	      {
+	        /* 将字符串透过calQrcode函数转换成boolean数组 */
+	        boolean[][] bEncoding = testQrcode.calQrcode(bytesEncoding);
+	        /* 依据编码后的boolean数组，绘图 */
+	        DrawQRCode(bEncoding, getResources().getColor(R.drawable.black));
+	      }
+	    }
+	    catch (Exception e)
+	    {
+	      Log.i("HIPPO", Integer.toString(QRcode.getText().length()) );
+	      e.printStackTrace();
+	    }
+	 }
+	
+
+
+	private void DrawQRCode(boolean[][] bRect, int colorFill) {
+		// TODO Auto-generated method stub
+		/* test Canvas*/
+	    int intPadding = 20;
+	    
+	    /* 欲在SurfaceView上绘图，需先lock锁定SurfaceHolder */
+	    Canvas mCanvas01 = mSurfaceHolder01.lockCanvas();
+	    
+	    /* 设定画布绘制颜色 */
+	    mCanvas01.drawColor(getResources().getColor(R.drawable.white));
+	    
+	    /* 建立画笔 */
+	    Paint mPaint01 = new Paint();
+	    
+	    /* 设定画笔颜色及样式 */
+	    mPaint01.setStyle(Paint.Style.FILL);
+	    mPaint01.setColor(colorFill);
+	    mPaint01.setStrokeWidth(1.0F);
+	    
+	    /* 逐一加载2维boolean数组 */
+	    for (int i=0;i<bRect.length;i++)
+	    {
+	      for (int j=0;j<bRect.length;j++)
+	      {
+	        if (bRect[j][i])
+	        {
+	          /* 依据数组值，绘出条形码方块 */
+	          mCanvas01.drawRect(new Rect( intPadding+j*9+6,intPadding+i*9+6, intPadding+j*9+6+9, intPadding+i*9+6+9), mPaint01);
+	        }
+	      }
+	    }
+	    mSurfaceHolder01.unlockCanvasAndPost(mCanvas01);
 	}
 
 	@Override
@@ -33,14 +178,16 @@ public class C_mem_center extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
+	} 
+	
 	public void c_mod_inform_onClick(View v) {
 	    Intent intent = new Intent();  
 	    intent.setClass(C_mem_center.this,C_mod_inform.class);
 	   startActivity(intent);    //牟祇传
 	   finish();   //挡セ
- }
-public boolean onKeyDown(int keyCode, KeyEvent event) {
+    }
+	
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         
         if (keyCode == KeyEvent.KEYCODE_BACK)
         {
@@ -56,5 +203,26 @@ public boolean onKeyDown(int keyCode, KeyEvent event) {
         
         return super.onKeyDown(keyCode, event);
     }
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
+
+
+
