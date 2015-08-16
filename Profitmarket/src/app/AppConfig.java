25f4ed0d132c96d54f903a7966505e5f7e19cf31
jason <jason@jason-PC>
2015-08-16 23:51:32 +0800

    abc145555

--- Profitmarket/src/app/AppConfig.java
+++ Profitmarket/src/app/AppConfig.java
@@ -2,8 +2,8 @@ package app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.103/android_login_api/";
+    public static String URL_LOGIN = "http://192.168.0.105/android_login_api/";
  
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.103/android_login_api/";
+    public static String URL_REGISTER = "http://192.168.0.105/android_login_api/";
 }
--- Profitmarket/src/app/AppConfig_Stores.java
+++ Profitmarket/src/app/AppConfig_Stores.java
@@ -2,8 +2,8 @@ package app;
 
 public class AppConfig_Stores {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.103/android_login_stores/";
+    public static String URL_LOGIN = "http://192.168.0.105/android_login_stores/";
  
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.103/android_login_stores/";
+    public static String URL_REGISTER = "http://192.168.0.105/android_login_stores/";
 }
