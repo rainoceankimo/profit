72f914df8415ed76c8fd6eba26d5b99b241964c3
iok <iok@iok-PC>
2015-08-26 16:26:16 +0800

    iok826426

--- Profitmarket/src/com/example/profitmarket/S_SettleaCcounts.java
+++ Profitmarket/src/com/example/profitmarket/S_SettleaCcounts.java
@@ -1,124 +1,23 @@
 package com.example.profitmarket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import org.apache.http.NameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.example.profitmarket.AllProductsActivity.LoadAllProducts;
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.content.Intent;
 
-public class S_SettleaCcounts extends ListActivity {
-	
-	 private ListView listInput;
-	
-    public static ArrayList<String> list =new ArrayList<String>();
-	
-	
-	
-	//String[] items = {"1","2","3","4","5","6","7","8","9","10"};
-	
-	 private ArrayAdapter<String> adapter;
-	 
-		
-	//Spinner spinner2;
-	//TextView textView3;
-	//private ProgressDialog pDialog;
-	 
-    // Creating JSON Parser object
-    //JSONParser jParser = new JSONParser();
- 
-   // ArrayList<HashMap<String, String>> productsList;
- 
-    // url to get all products list
-   // private static String url_all_products = "http://10.3.204.1/android_connect/get_all_products.php";
- 
-    // JSON Node names
-   // private static final String TAG_SUCCESS = "success";
-   // private static final String TAG_PRODUCTS = "products";
-   // private static final String TAG_PID = "pid";
-   // private static final String TAG_NAME = "name";
-    //private static final String TAG_PRICE = "price";
-    // products JSONArray
-    //JSONArray products = null;
+
+public class S_SettleaCcounts extends Activity {
+
  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.s_settleaccounts);
 		
-		
-		//spinner2 = (Spinner) findViewById(R.id.spinner2);
-		//textView3 = (TextView) findViewById(R.id.textView3);
-	
-		//ArrayAdapter adapter = new ArrayAdapter(S_SettleaCcounts.this, android.R.layout.simple_spinner_item, new String[]{"1","2","3","4","5","6","7","8","9","10"});
-		//adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		//this.spinner2 = (Spinner)findViewById(R.id.spinner2);
-		//textView3=(TextView) findViewById(R.id.textView3);
-		//spinner2.setAdapter(adapter);
-		listInput = (ListView)findViewById(android.R.id.list);
-		//	items = new ArrayList<String>();
-			
-		adapter = new ArrayAdapter(S_SettleaCcounts.this, android.R.layout.simple_list_item_1, list);
-		listInput.setAdapter(adapter);
-		
-		
-		
-		
-		
-		
-		
-		//spinner2.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-			//@Override
-			//public void onItemSelected(AdapterView adapterView , View view, int position, long id) {
-				// TODO Auto-generated method stub
-				
-			//	String a = items[position];
-			//	textView3.setText(spinner2.getSelectedItem().toString());
-				
-			
-
-		//	@Override
-			//public void onNothingSelected(AdapterView arg0) {
-				// TODO Auto-generated method stub
-				
-		//	}
-		//});//*/
-		 // productsList = new ArrayList<HashMap<String, String>>();
-	        
-	        // Loading products in Background Thread
-	       // new LoadAllProducts().execute();
-	 
-	        // Get listview
-	       // ListView lv = getListView();
 	}
 	
 
@@ -168,96 +67,7 @@ public class S_SettleaCcounts extends ListActivity {
 		finish();   //µ²§ô¥»­¶
 	}
     
-    /*class LoadAllProducts extends AsyncTask <String, String, String> {
-    	 
-        /**
-         * Before starting background thread Show Progress Dialog
-         * 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(S_SettleaCcounts.this);
-           
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
- */
-        /*
-         * getting All products from url
-         * 
-        protected String doInBackground(String... args) {
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            // getting JSON string from URL
-            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
- 
-            // Check your log cat for JSON reponse
-            Log.d("All Products: ", json.toString());
- 
-            try {
-                // Checking for SUCCESS TAG
-                int success = json.getInt(TAG_SUCCESS);
- 
-                if (success == 1) {
-                    // products found
-                    // Getting Array of Products
-                    products = json.getJSONArray(TAG_PRODUCTS);
- 
-                    // looping through All Products
-                    for (int i = 0; i < products.length(); i++) {
-                        JSONObject c = products.getJSONObject(i);
- 
-                        // Storing each json item in variable
-                        String id = c.getString(TAG_PID);
-                        String name = c.getString(TAG_NAME);
-                        String price = c.getString(TAG_PRICE);
-                        // creating new HashMap
-                        HashMap<String, String> map = new HashMap<String, String>();
- 
-                        // adding each child node to HashMap key => value
-                        map.put(TAG_PID, id);
-                        map.put(TAG_NAME, name);
-                        map.put(TAG_PRICE, price);
-                        // adding HashList to ArrayList
-                        productsList.add(map);
-                    }
-                } else {
-                    // no products found
-                    // Launch Add New product Activity
-                    Intent i = new Intent(getApplicationContext(),
-                            NewProductActivity.class);
-                    // Closing all previous activities
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
- 
-            return null;
-        }
- 
-        
-         * After completing background task Dismiss the progress dialog
-         * *
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog after getting all products
-            pDialog.dismiss();
-            // updating UI from Background Thread
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    /**
-                     * Updating parsed JSON data into ListView
-                     * 
-                    ListAdapter adapter = new SimpleAdapter(
-                            S_SettleaCcounts.this, productsList,
-                            R.layout.list_item2, new String[] { TAG_PID,
-                                    TAG_NAME,TAG_PRICE},
-                            new int[] { R.id.pid, R.id.name ,R.id.price});
-                    // updating listview
-                    setListAdapter(adapter);
-                }*/
+
             
 
 }
